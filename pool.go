557e22a2f249e3923b4243e53499c7a535725eb9
govindansriram <sriramgovindanwork@gmail.com>
2024-08-19 18:19:32 -0700

    renamed data

--- pool.go
+++ pool.go
@@ -107,21 +107,21 @@ func (s *connectionPool) len() (int, error) {
 	return int(lth), nil
 }
 
-type SubscriberClient struct {
-	cPool connectionPool
-}
-
 type PublisherClient struct {
 	cPool connectionPool
 }
 
-func NewSubscriberClient(
+type SubscriberClient struct {
+	cPool connectionPool
+}
+
+func NewPublisherClient(
 	username,
 	password string,
 	maxConnections,
 	maxIoTimeSeconds,
 	port uint16,
-	address string) SubscriberClient {
+	address string) PublisherClient {
 
 	cp := connectionPool{
 		username:            username,
@@ -133,67 +133,6 @@ func NewSubscriberClient(
 		isPub:               false,
 		port:                port,
 		address:             address,
-	}
-
-	sc := SubscriberClient{
-		cPool: cp,
-	}
-
-	go func() {
-		sc.cPool.connectionWorker()
-	}()
-
-	return sc
-}
-
-func (sc SubscriberClient) Len() (int, error) {
-	return sc.cPool.len()
-}
-
-func (sc SubscriberClient) Push(mess []byte) (int, error) {
-	conn, err := sc.cPool.getConnection()
-	if err != nil {
-		return -1, err
-	}
-
-	pos, alive, err := push(conn, time.Second*time.Duration(sc.cPool.maxIoTimeSeconds), mess)
-	deleteMessage(mess)
-
-	if !alive {
-		closeConn(conn)
-		<-sc.cPool.utilizedConnections
-		return -1, err
-	}
-
-	sc.cPool.readyConnections <- conn
-
-	if err != nil {
-		return -1, err
-	}
-
-	return int(pos), nil
-}
-
-func NewPublisherClient(
-	username,
-	password string,
-	maxConnections,
-	maxIoTimeSeconds,
-	maxPollingTimeSeconds,
-	port uint16,
-	address string) PublisherClient {
-
-	cp := connectionPool{
-		username:              username,
-		password:              password,
-		maxIoTimeSeconds:      maxIoTimeSeconds,
-		maxPollingTimeSeconds: maxPollingTimeSeconds,
-		kill:                  make(chan struct{}),
-		utilizedConnections:   make(chan struct{}, maxConnections),
-		readyConnections:      make(chan net.Conn, maxConnections),
-		isPub:                 true,
-		port:                  port,
-		address:               address,
 	}
 
 	sc := PublisherClient{
@@ -211,16 +150,77 @@ func (pc PublisherClient) Len() (int, error) {
 	return pc.cPool.len()
 }
 
-func (pc PublisherClient) hideOrPoll(hiddenTime uint16, isPoll bool) (uuid.UUID, []byte, error) {
+func (pc PublisherClient) Push(mess []byte) (int, error) {
 	conn, err := pc.cPool.getConnection()
+	if err != nil {
+		return -1, err
+	}
 
-	totalTime := uint32(pc.cPool.maxIoTimeSeconds)
+	pos, alive, err := push(conn, time.Second*time.Duration(pc.cPool.maxIoTimeSeconds), mess)
+	deleteMessage(mess)
+
+	if !alive {
+		closeConn(conn)
+		<-pc.cPool.utilizedConnections
+		return -1, err
+	}
+
+	pc.cPool.readyConnections <- conn
+
+	if err != nil {
+		return -1, err
+	}
+
+	return int(pos), nil
+}
+
+func NewSubscriberClient(
+	username,
+	password string,
+	maxConnections,
+	maxIoTimeSeconds,
+	maxPollingTimeSeconds,
+	port uint16,
+	address string) SubscriberClient {
+
+	cp := connectionPool{
+		username:              username,
+		password:              password,
+		maxIoTimeSeconds:      maxIoTimeSeconds,
+		maxPollingTimeSeconds: maxPollingTimeSeconds,
+		kill:                  make(chan struct{}),
+		utilizedConnections:   make(chan struct{}, maxConnections),
+		readyConnections:      make(chan net.Conn, maxConnections),
+		isPub:                 true,
+		port:                  port,
+		address:               address,
+	}
+
+	sc := SubscriberClient{
+		cPool: cp,
+	}
+
+	go func() {
+		sc.cPool.connectionWorker()
+	}()
+
+	return sc
+}
+
+func (sc SubscriberClient) Len() (int, error) {
+	return sc.cPool.len()
+}
+
+func (sc SubscriberClient) hideOrPoll(hiddenTime uint16, isPoll bool) (uuid.UUID, []byte, error) {
+	conn, err := sc.cPool.getConnection()
+
+	totalTime := uint32(sc.cPool.maxIoTimeSeconds)
 	if err != nil {
 		return [16]byte{}, nil, err
 	}
 
 	if isPoll {
-		totalTime += uint32(pc.cPool.maxPollingTimeSeconds)
+		totalTime += uint32(sc.cPool.maxPollingTimeSeconds)
 	}
 
 	uid, mess, alive, err := hideAndPoll(
@@ -231,11 +231,11 @@ func (pc PublisherClient) hideOrPoll(hiddenTime uint16, isPoll bool) (uuid.UUID,
 
 	if !alive {
 		closeConn(conn)
-		<-pc.cPool.utilizedConnections
+		<-sc.cPool.utilizedConnections
 		return [16]byte{}, nil, err
 	}
 
-	pc.cPool.readyConnections <- conn
+	sc.cPool.readyConnections <- conn
 
 	if err != nil {
 		return [16]byte{}, nil, err
@@ -244,32 +244,32 @@ func (pc PublisherClient) hideOrPoll(hiddenTime uint16, isPoll bool) (uuid.UUID,
 	return uid, mess, nil
 }
 
-func (pc PublisherClient) Hide(hiddenTime uint16) (uuid.UUID, []byte, error) {
-	return pc.hideOrPoll(hiddenTime, false)
+func (sc SubscriberClient) Hide(hiddenTime uint16) (uuid.UUID, []byte, error) {
+	return sc.hideOrPoll(hiddenTime, false)
 }
 
-func (pc PublisherClient) Poll(hiddenTime uint16) (uuid.UUID, []byte, error) {
-	return pc.hideOrPoll(hiddenTime, true)
+func (sc SubscriberClient) Poll(hiddenTime uint16) (uuid.UUID, []byte, error) {
+	return sc.hideOrPoll(hiddenTime, true)
 }
 
-func (pc PublisherClient) Delete(uid uuid.UUID) (uuid.UUID, error) {
-	conn, err := pc.cPool.getConnection()
+func (sc SubscriberClient) Delete(uid uuid.UUID) (uuid.UUID, error) {
+	conn, err := sc.cPool.getConnection()
 	if err != nil {
 		return [16]byte{}, err
 	}
 
 	uid, alive, err := del(
 		conn,
-		time.Duration(pc.cPool.maxPollingTimeSeconds)*time.Second,
+		time.Duration(sc.cPool.maxPollingTimeSeconds)*time.Second,
 		uid)
 
 	if !alive {
 		closeConn(conn)
-		<-pc.cPool.utilizedConnections
+		<-sc.cPool.utilizedConnections
 		return [16]byte{}, err
 	}
 
-	pc.cPool.readyConnections <- conn
+	sc.cPool.readyConnections <- conn
 
 	if err != nil {
 		return [16]byte{}, err
